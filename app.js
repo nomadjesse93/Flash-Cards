f477ee4c7c65e0c13bd1a1349d841a168024c8eb
Jesse Sanderson <jesse93@protonmail.com>
2019-07-12 22:27:12 -0600

    created "includes folder", experimented with conditionals in pug

--- app.js
+++ app.js
@@ -2,14 +2,50 @@ const express = require('express');
 
 const app = express();
 
+const firstNames = ['Dwayne', 'Dana', 'Jesse', 'John', 'Billy']
+
+const lastNames = ['Johnson', 'Hodges', 'Sanderson', 'Cena', 'Eilish']
+
+const sandboxNames = [{
+        First: 'Paul',
+        Last: 'Jones'
+    },
+    {
+        First: 'David',
+        Last: 'Smith'
+    },
+    {
+        First: 'Jason',
+        Last: 'Camp'
+    },
+    {
+        First: 'Bella'
+    },
+    {
+        Last: 'Jones'
+    }
+];
+
 app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
     res.render('index')
 })
 app.get('/cards', function (req, res) {
-    res.locals.prompt = "Who is buried in Grant's tomb?"
-    res.locals.hint = "Think about who's tomb it is!"
-    res.render('cards');
+
+    res.render('cards', {
+        prompt: "Who is buried in Grant's tomb?",
+        hint: "Think about who's tomb it is!"
+    });
+})
+
+//Sandbox
+//Firstname | Lastname + 5 names 
+app.get('/sandbox', (req, res) => {
+    res.render('sandbox', {
+        firstNames,
+        lastNames,
+        sandboxNames
+    })
 })
 app.listen(3000);
