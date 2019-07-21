d7f18ad1f0cdcba62535174338848332fd73224e
Jesse Sanderson <jesse93@protonmail.com>
2019-07-21 10:30:48 -0600

    added route and error logic

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 //Middleware
+//You end middle ware calls by either sending a response
+//to the client or calling next()
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
@@ -8,10 +11,15 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 
+app.use(cookieParser());
+
 app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
-    res.render('index')
+    const name = req.cookies.username;
+    name ? res.render('index', {
+        name
+    }) : res.redirect('hello');
 });
 
 app.get('/cards', function (req, res) {
@@ -23,14 +31,31 @@ app.get('/cards', function (req, res) {
 });
 
 app.get('/hello', (req, res) => {
-    res.render('hello')
-})
+    const name = req.cookies.username
+    name ? res.redirect('/') : res.render('hello')
+});
 
 app.post('/hello', (req, res) => {
-    res.render('hello', {
-        name: req.body.username
-    });
+    res.cookie('username', req.body.username)
+    res.redirect('/')
+});
+
+app.post('/goodbye', (req, res) => {
+    res.clearCookie('username')
+    res.redirect('hello')
+});
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found')
+    err.status = 404;
+    next(err);
 })
 
+app.use((err, req, res, next) => {
+    res.status(err.status);
+    res.render('error', {
+        error: err
+    });
+});
 
 app.listen(3000);
